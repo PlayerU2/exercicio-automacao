91ea335a36f27aa40460b78249124cf708f9b7e7
Flávio Araujo <flavio21259@gmail.com>
2021-08-23 13:13:36 -0300

    usando massa de dados

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,11 +1,11 @@
 /// <reference types="cypress" />
-
+const perfil = require('../fixtures/perfil.json')
 
 context('Funcionalidade Login', () =>{
 
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     afterEach(() => {
@@ -22,6 +22,26 @@ context('Funcionalidade Login', () =>{
         cy.get('a > .hidden-xs').should('contain' , 'Welcome aluno_ebac !')
         
     })
+
+    it('Deve fazer login com sucesso - Usando arquivo de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+
+
+        cy.get('.topbar-inner > :nth-child(1) > .list-inline > :nth-child(2) > a').should('contain' , 'Logout')
+    });
+
+    it('Deve fazer login com sucesso - Usando fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, {log: false})
+            cy.get('.woocommerce-form > .button').click()
+    
+    
+            cy.get('.topbar-inner > :nth-child(1) > .list-inline > :nth-child(2) > a').should('contain' , 'Logout')
+        })
+    });
 
 
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () =>{
